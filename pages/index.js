175e09bd1ba2c794e29ec922172ee05497fd2057
IsabellaSoares <isabella.soares.28@gmail.com>
2021-01-25 15:42:37 -0300

    update GitHub url

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/IsabellaSoares/harry-potter-quiz" />
     </QuizBackground>
   );
 }
