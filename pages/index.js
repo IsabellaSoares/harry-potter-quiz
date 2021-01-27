9a86eab3604aab6ddef8d0d83838db00ca580788
IsabellaSoares <isabella.soares.28@gmail.com>
2021-01-27 10:13:58 -0300

    init Harry Potter layout

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
-  margin: auto 10%;
+  margin: auto 50%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
